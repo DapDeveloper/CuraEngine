beed86afe6e3960830a682badb679d05a6d0e845
DapDeveloper <danielantoninpetrea@gmail.com>
2019-05-10 16:19:58 +0200

    update

--- src/gcodeExport.cpp
+++ src/gcodeExport.cpp
@@ -232,7 +232,10 @@ std::string GCodeExport::getFileHeader(const std::vector<bool>& extruder_is_used
         break;
     default:
         prefix << ";FLAVOR:" << flavorToString(flavor) << new_line;
-        prefix << ";TIME:" << ((print_time)? static_cast<int>(*print_time) : 6666) << new_line;
+        int hours=print_time/60
+        int minutes=print_time%60
+        //prefix << ";TIME:" << ((print_time)? static_cast<int>(*print_time) : 6666) << new_line;
+        prefix <<";   Build time: "<<hours<<" hours "<<minutes<<" minutes"<<new_line;
         if (flavor == EGCodeFlavor::ULTIGCODE)
         {
             prefix << ";MATERIAL:" << ((filament_used.size() >= 1)? static_cast<int>(filament_used[0]) : 6666) << new_line;
