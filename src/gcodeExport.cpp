3fab2f3779d82446d5b787fd84e25c24d339d83b
DapDeveloper <danielantoninpetrea@gmail.com>
2019-05-10 16:33:09 +0200

    update

--- src/gcodeExport.cpp
+++ src/gcodeExport.cpp
@@ -232,9 +232,9 @@ std::string GCodeExport::getFileHeader(const std::vector<bool>& extruder_is_used
         break;
     default:
         prefix << ";FLAVOR:" << flavorToString(flavor) << new_line;
-        hours=static_cast<int>print_time;
+        hours=static_cast<int>(*print_time);
         hours=hours/60;
-        minutes=static_cast<int>print_time;
+        minutes=static_cast<int>(*print_time);
         minutes=minutes%60;
         //prefix << ";TIME:" << ((print_time)? static_cast<int>(*print_time) : 6666) << new_line;
         prefix <<";   Build time: "<<hours<<" hours "<<minutes<<" minutes"<<new_line;
